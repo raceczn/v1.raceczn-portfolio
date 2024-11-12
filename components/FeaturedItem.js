dabf24e2803b2043cabe7e1cb20493e815a0424d
raceczn <rancescuizon@gmail.com>
2024-11-13 01:50:14 +0800

    m

--- components/FeaturedItem.js
+++ components/FeaturedItem.js
@@ -3,14 +3,28 @@ import { motion } from "framer-motion";
 import { slideUp } from "../src/utils/animationVariants";
 import Button from "./Button";
 import { FiGithub } from "react-icons/fi";
-import { FiGlobe } from "react-icons/fi"; 
+import { FiGlobe } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
 const FeaturedItem = ({ featuredProject }) => {
+  // State to track if the component has mounted
+  const [mounted, setMounted] = useState(false);
+
+  // Set mounted to true once the component is mounted
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // If the component hasn't mounted yet, return null to prevent hydration mismatch
+  if (!mounted) {
+    return null; // Avoid rendering on the server side
+  }
+
   const { title, subtitle, body, tools, github, website, image } = featuredProject.fields;
 
   // Function to check if the URL is a GitHub link
   const isGithubUrl = (url) => {
-    return url.toLowerCase().includes('github');
+    return url.toLowerCase().includes("github");
   };
 
   return (
@@ -18,11 +32,11 @@ const FeaturedItem = ({ featuredProject }) => {
       <div className="featured__img-container">
         {image && (
           <Image
-            src={image.fields.file.src}  // Use the path from the project data
+            src={image.fields.file.src} // Use the path from the project data
             alt={title}
             width={image.fields.file.details.image.width}
             height={image.fields.file.details.image.height}
-            className="slanted-image"  // Added the class for slant effect
+            className="slanted-image" // Added the class for slant effect
           />
         )}
       </div>
@@ -48,13 +62,13 @@ const FeaturedItem = ({ featuredProject }) => {
               text="Take a look"
             />
           ) : (
-            <span className="btn btn__primary not-deployed">Not deployed</span>  
+            <span className="btn btn__primary not-deployed">Not deployed</span>
           )}
           <Button
-            href={github}  
+            href={github}
             className="btn btn__link"
             target="_blank"
-            text={isGithubUrl(github) ? <FiGithub /> : <FiGlobe />}  
+            text={isGithubUrl(github) ? <FiGithub /> : <FiGlobe />}
           />
         </div>
       </div>
